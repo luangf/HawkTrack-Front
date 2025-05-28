5b3b714acc07fe16a86e24d20be213b1dbaa226e
luangf <luangf222@gmail.com>
2025-05-28 12:52:49 -0300

    feat: add sidebar open/closed state using cookies

--- src/pages/private/SystemLayout.tsx
+++ src/pages/private/SystemLayout.tsx
@@ -4,12 +4,15 @@ import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Outlet } from "react-router-dom";
 import DashboardMenu from "../../components/general/DashboardMenu";
+import Cookies from "js-cookie";
 
 export default function SystemLayout() {
+  const defaultOpen = Cookies.get("sidebar_state") === "true";
+  
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Toaster richColors position="top-center" />
-      <SidebarProvider className="flex-col">
+      <SidebarProvider className="flex-col" defaultOpen={defaultOpen}>
         <DashboardMenu />
         <AppSidebar />
         <Outlet />
